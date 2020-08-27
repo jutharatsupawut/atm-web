22a8402fad9698f398e71afa68c87421e43b4c02
จุฑารัตน์ แซ่เนี้ยว <jutharat.sa@rd.go.th>
2020-08-27 14:30:32 +0700

    connect DB

--- src/main/java/th/go/rd/atm/service/CustomerService.java
+++ src/main/java/th/go/rd/atm/service/CustomerService.java
@@ -1,5 +1,6 @@
 package th.go.rd.atm.service;
 
+import org.mindrot.jbcrypt.BCrypt;
 import org.springframework.stereotype.Service;
 import th.go.rd.atm.model.Customer;
 
@@ -18,16 +19,44 @@ public class CustomerService {
        customerList = new ArrayList<>();
 
    }
-public void createCustomer(Customer customer){
 
+    public void createCustomer(Customer customer){
        // hash pin code area
-       customerList.add(customer);
+    String hashPin = hash(customer.getPin());
+    customer.setPin(hashPin);
+    customerList.add(customer);
 
   }
 
-  public List<Customer> getCustomers(){
-
+     public List<Customer> getCustomers(){
        return new ArrayList<>(this.customerList);
   }
+
+     public Customer findCustomer(int id){
+
+       for (Customer c : customerList){
+           if (c.getId() == id)
+               return c;
+       }
+       return null;
+  }
+
+
+      public Customer checkPin(Customer inputCustomer){
+       Customer storedCustomer = findCustomer(inputCustomer.getId());
+
+       if (storedCustomer != null){
+           String hashPin = storedCustomer.getPin();
+              if (BCrypt.checkpw(inputCustomer.getPin(), hashPin))
+                  return storedCustomer;
+
+      }
+      return null;
+}
+
+      private String hash(String pin) {
+        String salt = BCrypt.gensalt(12);
+        return BCrypt.hashpw(pin, salt);
+    }
 
 }
